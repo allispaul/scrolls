ea24274e5581a36da81b484812b9b8e5e432ba55
Paul VanKoughnett <allispaul@gmail.com>
2023-05-25 00:15:18 -0500

    add SubvolumeDataset, and methods get_train_and_val_dsets() and show_labels_with_rects() for defining and visualizing datasets, to utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,210 @@
+from pathlib import Path
+from typing import Optional, List, Tuple
+
+from PIL import Image
+# disable PIL.DecompressionBombWarning
+Image.MAX_IMAGE_PIXELS = None
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from sklearn.metrics import fbeta_score
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
-from tqdm import tqdm
-from typing import Optional
+
+from tqdm.auto import tqdm
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+def get_train_and_val_dsets(
+    fragments_to_use: List[int],
+    data_path: Path | str,
+    z_start: int | List[int],
+    z_dim: int,
+    buffer: int,
+    validation_rects: List[Tuple[int]],
+    shuffle: bool = True,
+    amount_of_data: int | float | None = None,
+) -> Tuple[data.Dataset, data.Dataset]:
+    """Get training and validation datasets from a list of fragments.
+    
+    Construct two datasets from the fragments named in fragments_to_use. The
+    items in each dataset are cuboidal subvolumes of length 2*buffer+1 in the
+    x and y directions and length z_dim in the z direction. A rectangular chunk
+    is held out of the training set for each fragment, and used to define the
+    validation set. The datasets can optionally be shuffled and restricted in
+    size.
+    
+    Args:
+      fragments_to_use: List of fragments to include in the dataset. All
+        entries should be in {1, 2, 3}.
+      data_path: Path to data folder.
+      z_start: Lowest z-value to use from each fragment. Should be between 0
+        and 64. If an int, the same z-value is used for all fragments; if a
+        list, z_start[i] is used for fragments_to_use[i].
+      z_dim: Number of z-values to use from each fragment. This should be the
+        same for all fragments so that the model can work with data of constant
+        shape.
+      buffer: Radius of subvolumes in x and y directions. Thus, each item in
+        the dataset will be a subvolume of size 2*buffer+1 x 2*buffer+1 x z_dim.
+      validation_rect: Rectangle to hold out from each fragment to form the
+        validation set. Each rectangle is a tuple in the format:
+          (top_left_corner_x, top_left_corner_y, width, height)
+        Use show_labels_with_rects() to double-check the rectangles.
+      shuffle: Whether to shuffle the datasets before returning (default True).
+      amount_of_data: If None, return the whole dataset. If an int, return a
+        training dataset with that many items, and a proportionally long
+        validation dataset. If a float, return that fraction of the total
+        training and validation datasets (it must be between 0 and 1). You
+        should probably use shuffle=True if you want to use this parameter.
+    
+    Returns:
+      A tuple (train_dset, val_dset) of the training and validation datasets.
+    
+    Example usage:
+      train_dset, val_dset = get_train_and_val_dsets(
+          fragments_to_use=[1, 2, 3],
+          data_path='data',
+          z_start=16,
+          z_dim=32,
+          buffer=30,
+          validation_rects=[(1100, 3500, 700, 950), ...],
+          shuffle=True,
+          amount_of_data: 0.1)
+    """
+    data_path = Path(data_path)
+    train_path = data_path / "train"
+    
+    if isinstance(z_start, int):
+        z_start = [z_start for _ in fragments_to_use]
+    
+    train_dsets = []
+    val_dsets = []
+    for i, fragment in enumerate(fragments_to_use):
+        # Path for ith fragment
+        prefix = train_path / str(fragment)
+        
+        # read images
+        images = [
+            np.array(Image.open(filename), dtype=np.float32)/65535.0
+            for filename
+            in tqdm(sorted((prefix / "surface_volume").glob("*.tif"))[
+                z_start[i] : z_start[i] + z_dim])
+        ]
+        
+        # turn images to tensors
+        image_stack = torch.stack([torch.from_numpy(image) for image in images], 
+                                  dim=0)
+
+        # get mask and labels
+        mask = np.array(Image.open(prefix / "mask.png").convert('1'))
+        label = torch.from_numpy(
+            np.array(Image.open(prefix / "inklabels.png"))
+        ).float()
+
+        # Split our dataset into train and val. The pixels inside the rect are the 
+        # val set, and the pixels outside the rect are the train set.
+        # Adapted from https://www.kaggle.com/code/jamesdavey/100x-faster-pixel-coordinate-generator-1s-runtime
+        
+        # Create a Boolean array of the same shape as the bitmask, initially all True
+        not_border = np.zeros(mask.shape, dtype=bool)
+        not_border[buffer:mask.shape[0]-buffer, buffer:mask.shape[1]-buffer] = True
+        arr_mask = mask * not_border
+        # define validation rectangle
+        rect = validation_rects[i]
+        inside_rect = np.zeros(mask.shape, dtype=bool) * arr_mask
+        # Sets all indexes with inside_rect array to True
+        inside_rect[rect[1]:rect[1]+rect[3]+1, rect[0]:rect[0]+rect[2]+1] = True
+        # Set the pixels within the inside_rect to False
+        outside_rect = np.ones(mask.shape, dtype=bool) * arr_mask
+        outside_rect[rect[1]:rect[1]+rect[3]+1, rect[0]:rect[0]+rect[2]+1] = False
+        pixels_inside_rect = torch.tensor(np.argwhere(inside_rect))
+        pixels_outside_rect = torch.tensor(np.argwhere(outside_rect))
+
+        # define datasets
+        train_dset = SubvolumeDataset(image_stack, label, pixels_outside_rect, buffer, z_dim)
+        val_dset = SubvolumeDataset(image_stack, label, pixels_inside_rect, buffer, z_dim)
+        train_dsets.append(train_dset)
+        val_dsets.append(val_dset)
+        
+    # Concatenate datasets
+    train_dset = data.ConcatDataset(train_dsets)
+    val_dset = data.ConcatDataset(val_dsets)
+    
+    # Find desired lengths of datasets to return
+    train_length = len(train_dset)
+    val_length = len(val_dset)
+    if amount_of_data is None:
+        desired_train_length = train_length
+        desired_val_length = val_length
+    elif isinstance(amount_of_data, int):
+        desired_train_length = amount_of_data
+        desired_val_length = int(desired_train_length / train_length * val_length)
+    elif isinstance(amount_of_data, float):
+        desired_train_length = int(amount_of_data * train_length)
+        desired_val_length = int(amount_of_data * val_length)
+    else:
+        raise TypeError(f"Bad type <{type(amount_of_data)}> for amount_of_data: expected None, int, or float")
+    
+    # Return subsets of appropriate length
+    if not shuffle:
+        return (data.Subset(train_dset, range(desired_train_length)),
+                data.Subset(val_dset, range(desired_val_length)))
+    return (data.Subset(train_dset, torch.randperm(train_length)[:desired_train_length]),
+            data.Subset(val_dset, torch.randperm(val_length)[:desired_val_length]))
+            
+            
+def show_labels_with_rects(
+    fragments_to_use: List[int],
+    data_path: Path | str,
+    validation_rects: List[Tuple[int]],
+    ):
+    """Plot the inklabels of given fragments, with validation rects highlighted.
+    
+    Args:
+      fragments_to_use: List of fragments to show. All entries should be in 
+        {1, 2, 3}.
+      data_path: Path to data folder.
+      validation_rect: Rectangle to highlight on each fragment. Each rectangle
+        is a tuple in the format:
+          (top_left_corner_x, top_left_corner_y, width, height)
+    """
+    data_path = Path(data_path)
+    train_path = data_path / "train"
+    
+    for i, fragment in enumerate(fragments_to_use):
+        plt.subplot(1, len(fragments_to_use), i+1) 
+        label = np.array(Image.open(train_path / str(fragment) / "inklabels.png"))
+        plt.imshow(label, cmap="gray")
+        rect = validation_rects[i]
+        patch = patches.Rectangle((rect[0], rect[1]), rect[2], rect[3], linewidth=2, edgecolor='r', facecolor='none')
+        ax = plt.gca()
+        ax.add_patch(patch)
+        ax.axis('off')
+
+
+class SubvolumeDataset(data.Dataset):
+    def __init__(self, image_stack, label, pixels, buffer, z_dim):
+        self.image_stack = image_stack
+        self.label = label
+        self.pixels = pixels
+        self.buffer = buffer
+        self.z_dim = z_dim
+    def __len__(self):
+        return len(self.pixels)
+    def __getitem__(self, index):
+        y, x = self.pixels[index]
+        subvolume = self.image_stack[
+            :, y-self.buffer:y+self.buffer+1, x-self.buffer:x+self.buffer+1
+        ].view(1, self.z_dim, self.buffer*2+1, self.buffer*2+1)
+        inklabel = self.label[y, x].view(1)
+        return subvolume, inklabel   
+    
+    
 # renamed from "visualize"
 class MetricsRecorder():
     def __init__(self):
@@ -26,7 +221,8 @@ class MetricsRecorder():
         labels = labels.to(DEVICE)
         pred_labels = outputs.detach().sigmoid().gt(0.4).int()
         accuracy = (pred_labels == labels).sum().float().div(labels.size(0)).cpu()
-        self.fbeta += fbeta_score(labels.view(-1).cpu().numpy(), pred_labels.view(-1).cpu().numpy(), beta=0.5)
+        self.fbeta += fbeta_score(labels.view(-1).cpu().numpy(),
+                                  pred_labels.view(-1).cpu().numpy(), beta=0.5)
         self.accuracy += accuracy.item()
         self.loss += loss.item()
     def reset(self):
@@ -35,6 +231,7 @@ class MetricsRecorder():
         self.loss = 0
         self.accuracy = 0
 
+        
 class Trainer():
     """Bundles together a model, a training and optionally a validation dataset,
     an optimizer, and loss/accuracy/fbeta@0.5 metrics. Stores histories of the
