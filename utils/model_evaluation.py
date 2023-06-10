8b71341a4226cd4529a09be463ef343b9f7e485b
Paul VanKoughnett <allispaul@gmail.com>
2023-06-09 23:04:06 -0500

    predict_validation_rects also returns loss, acc, fbeta

--- utils/model_evaluation.py
+++ utils/model_evaluation.py
@@ -19,29 +19,28 @@ from torch.utils.tensorboard import SummaryWriter
 
 from tqdm.auto import tqdm
 
-from .data_preparation import get_rect_dset
+from .data_preparation import get_rect_dset_small
+from .logging import MetricsRecorder
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def predict_validation_rects(model: nn.Module, 
-                             fragments_to_use: List[int],
-                             data_path: Path | str,
-                             z_start: int | List[int],
+                             fragments_to_use: list[Path | str],
+                             z_start: int | list[int],
                              z_dim: int,
                              buffer: int,
-                             validation_rects: List[Tuple[int]],
+                             validation_rects: list[tuple[int]],
                              decision_boundary: float = 0.4,
                              writer: SummaryWriter | None = None
-                            ) -> matplotlib.figure.Figure:
+                            ) -> tuple[matplotlib.figure.Figure,
+                                       float, float, float]:
     """Predict ink labels of given rectangles in the training fragments.
     Display them and add them to TensorBoard.
 
     Args:
       model: Model to use for prediction.
-      fragments_to_use: List of fragments to predict on. All entries should
-        be in {1, 2, 3}.
-      data_path: Path to data folder.
+      fragments_to_use: List of paths to fragments to predict on.
       z_start: Lowest z-value to use from each fragment. Should be between 0
         and 64. 
       z_dim: Number of z-values to use from each fragment.
@@ -55,11 +54,13 @@ def predict_validation_rects(model: nn.Module,
       writer: Optional SummaryWriter object used to add images to TensorBoard.
     
     Returns:
-      A matplotlib figure showing the predictions and the true ink labels.
+      A tuple containing: a matplotlib figure showing the predictions and the 
+      true ink labels, and the accuracy, loss, and F0.5 scores of the
+      predictions.
     """
     
     # clean inputs
-    data_path = Path(data_path)
+    fragments_to_use = [Path(x) for x in fragments_to_use]
     if isinstance(z_start, int):
         z_start = [z_start for _ in fragments_to_use]
     BATCH_SIZE = 32
@@ -72,30 +73,38 @@ def predict_validation_rects(model: nn.Module,
     subfigs[1].suptitle('Actual')
     axs_actual = subfigs[1].subplots(nrows=1, ncols=3)
     
+    criterion = nn.BCEWithLogitsLoss()
     model.eval()
 
     for i, (fragment, rect) in enumerate(zip(fragments_to_use, validation_rects)):
         # define validation dataset
-        val_dset = get_rect_dset_small(fragment, data_path, z_start[i], z_dim,
+        val_dset = get_rect_dset_small(fragment, z_start[i], z_dim,
                                        buffer, rect)
         
         val_loader = data.DataLoader(val_dset, batch_size=BATCH_SIZE, shuffle=False)
         
         # predict on validation dataset
         outputs = []
+        recorder = MetricsRecorder()
         with torch.no_grad():
-            for subvolumes, _ in tqdm(val_loader,
+            for subvolumes, inklabels in tqdm(val_loader,
                                       desc=f"Predicting on fragment {fragment}"):
-                output = model(subvolumes.to(DEVICE)).view(-1).sigmoid().cpu().numpy()
+                output = model(subvolumes.to(DEVICE)).view(-1).sigmoid().cpu()
                 outputs.append(output)
+                loss = criterion(inklabels, output.unsqueeze(1))
+                recorder.update(output, inklabels, loss)
         image_shape = val_dset.image_stack[0].shape
 
         # get boolean predictions using decision boundary
-        pred_image = np.zeros(image_shape, dtype=np.uint8)
-        outputs = np.concatenate(outputs)
+        pred_image = torch.zeros(image_shape, dtype=torch.uint8)
+        outputs = torch.cat(outputs)
         for (y, x), prob in zip(val_dset.pixels[:outputs.shape[0]], outputs):
             pred_image[y, x] = prob > decision_boundary
             
+        loss = recorder.loss / len(val_loader)
+        acc = recorder.accuracy / len(val_loader)
+        fbeta = recorder.fbeta / len(val_loader)
+        
         # clean up
         del val_dset
         del val_loader
@@ -108,7 +117,7 @@ def predict_validation_rects(model: nn.Module,
         axs_pred[i].axis('off')
 
         # plot actual labels
-        label = np.array(Image.open(data_path / "train" / f"{i+1}/inklabels.png"))
+        label = np.array(Image.open(fragment / "inklabels.png"))
         axs_actual[i].imshow(label, cmap='gray')
         axs_actual[i].set_xlim([rect[0], rect[0]+rect[2]])
         axs_actual[i].set_ylim([rect[1]+rect[3], rect[1]])
@@ -118,4 +127,4 @@ def predict_validation_rects(model: nn.Module,
     if writer is not None:
         writer.add_figure('Validation predictions', fig)
     
-    return fig
+    return fig, loss, acc, fbeta
